c672d579bdce4d9f0e49cc01d1e62a382c9c1d41
unknown <adriano.antunes@gmail.com>
2023-11-12 01:45:10 -0300

    Learn React in act

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+//https://www.youtube.com/watch?v=edBx-fjgh4k&t=1626s
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
